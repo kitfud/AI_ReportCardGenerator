5184db823b02cf3e31eaed21323bf5d223fbdce9
Kit Fuderich <kitfuderich@gmail.com>
2024-04-03 16:37:29 -0700

    various styling changes and improved landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,7 +1,16 @@
 import React, { useRef } from 'react';
-import { Button, Typography, Box } from '@mui/material';
+import { Button, Typography, Box,Card } from '@mui/material';
 import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import SchoolIcon from '@mui/icons-material/School';
+import { Link } from 'react-router-dom';
+
+import NoteAltTwoToneIcon from '@mui/icons-material/NoteAltTwoTone';
+import WorkHistoryRoundedIcon from '@mui/icons-material/WorkHistoryRounded';
+import TagFacesRoundedIcon from '@mui/icons-material/TagFacesRounded';
+
+import PublicIcon from '@mui/icons-material/Public';
+import CopyrightIcon from '@mui/icons-material/Copyright';
 
 const LandingPage = () => {
   const aboutRef = useRef(null);
@@ -30,43 +39,108 @@ const LandingPage = () => {
         margin:'auto',
         width:'90%',
         padding: '2rem' }}>
-        <Button variant="contained" onClick={() => scrollToSection(aboutRef)}>About Us</Button>
-        <Button variant="contained" onClick={() => scrollToSection(servicesRef)}>Services</Button>
-        <Button variant="contained" onClick={() => scrollToSection(contactRef)}>Contact</Button>
+        <Button sx={{margin:'2px'}} variant="contained" onClick={() => scrollToSection(aboutRef)}>About Us</Button>
+        <Button sx={{margin:'2px'}} variant="contained" onClick={() => scrollToSection(servicesRef)}>Services</Button>
+        <Button sx={{margin:'2px'}} variant="contained" onClick={() => scrollToSection(contactRef)}>Contact</Button>
       </Box>
       <Box ref={aboutRef} sx={{
+      display:'flex',
+      textAlign:'center',
       margin:'auto',
       padding: '2rem', 
       width:'90%',
       backgroundColor: '#e0e0e0',
-      height:'50vh'}}>
-        <Typography variant="h4">About Us</Typography>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, 
-          nunc ut aliquam tincidunt, nisl lectus feugiat enim, vitae mollis 
-          dolor diam vel odio. 
-        </Typography>
+      opacity:'60%',
+      height:'40vh'}}>
+        <Box sx={{width:'40%',margin:'auto'}}>
+        <Typography 
+        color="primary"
+        variant="h4"><u>About Us</u></Typography>
+        <Typography color="primary" variant='h6'>
+          A one stop shop for your teaching needs! Our product seeks to address some of the time crunches which happen throughout the school year with our AI enhanced productivity features. 
+        </Typography> 
+        </Box>
+     
+        <Box sx={{width:'40%',margin:'auto'}}>
+        <SchoolIcon color="primary" sx={{
+            fontSize:'250px'}}/>
+        </Box>
+      
       </Box>
       <Box ref={servicesRef} sx={{ padding: '2rem',
       margin:'auto',
       width:'90%',
-      height:'50vh' }}>
-        <Typography variant="h4">Services</Typography>
-        <Typography>
-          Donec ullamcorper nulla non metus auctor fringilla. 
-          Vestibulum id ligula porta felis euismod semper. 
+      height:'35vh' }}>
+        <Typography 
+        color="primary"
+        variant="h2" 
+        sx={{textAlign:'center',fontWeight:'bold'}} >Services</Typography>
+        <Typography 
+        color="primary"
+        sx={{textAlign:'center',fontWeight:'bold'}}>
+          Test our innovative products:
         </Typography>
+<Box sx={{display:'flex',marginTop:'20px',marginBottom:'20px'}}>
+<Card sx={{width:'33%',backgroundColor:'transparent',textAlign:'center'}}>
+<Link  style={{ color: 'white' }} to="/reportgenerator">
+<NoteAltTwoToneIcon className="linkHover" sx={{color:'darkgray',fontSize:'100px'}}/>
+<Typography className="linkHover" sx={{color:'darkgray'}} variant='h6'><u>Report Generator </u></Typography>
+</Link>
+<Box>
+  <Typography sx={{color:'darkgray'}} variant='h6'>Generate reports for your students based on a final grade or series of assignments.</Typography>
+</Box>
+</Card>
+<Card sx={{width:'33%',backgroundColor:'transparent',textAlign:'center'}}>
+<Link  style={{ color: 'white' }} to="/lessonplanner">
+<WorkHistoryRoundedIcon className="linkHover" sx={{color:'darkgray',fontSize:'100px'}}/>
+<Typography className="linkHover" sx={{color:'darkgray'}} variant='h6'><u>Lesson Planner </u></Typography>
+</Link>
+<Typography sx={{color:'darkgray'}} variant='h6'>Generate reports for your students based on a final grade or series of assignments.</Typography>
+</Card>
+<Card sx={{width:'33%',backgroundColor:'transparent',textAlign:'center'}}>
+<Link  style={{ color: 'white' }} to="/companion">
+<TagFacesRoundedIcon className="linkHover" sx={{color:'darkgray',fontSize:'100px'}}/>
+<Typography className="linkHover" sx={{color:'darkgray'}} variant='h6'><u>Constant Companion</u></Typography>
+</Link>
+<Typography sx={{color:'darkgray'}} variant='h6'>Generate reports for your students based on a final grade or series of assignments.</Typography>
+</Card>
+        </Box>
       </Box>
+
+      
       <Box ref={contactRef} sx={{ padding: '2rem', 
+      display:'flex',
       backgroundColor: '#e0e0e0',
       margin:'auto',
+      marginTop:'20px',
       width:'90%',
-      height:'50vh' }}>
-        <Typography variant="h4">Contact Us</Typography>
-        <Typography>
-          Praesent commodo cursus magna, vel scelerisque nisl consectetur et. 
-        </Typography>
-     
+      opacity:'60%',
+      height:'10vh',
+      textAlign:'center' }}>
+        
+        <Box sx={{width:'50%'}}>
+        <Typography 
+        color="primary"
+        variant="h6">Contact Us</Typography>
+        <PublicIcon
+          color="primary"
+          sx={{fontSize:'20px'}}
+          />
+          <Typography color="primary"
+          variant='h6'
+          >kitfuderich@email.com</Typography>
+        </Box>
+      
+        
+         <Box sx={{width:'50%',margin:'auto'}}>
+         <Typography 
+        color="primary"
+        variant="h6">Copyright</Typography>
+         <CopyrightIcon sx={{fontSize:'20px'}} color="primary"/>
+          <Typography variant='h6' color="primary">2024 Kit Fuderich</Typography>
+         </Box>
+         
+    
       </Box>
       <Box sx={{textAlign:'center',left:'90%',margin:'20px'}}>
       <Button 
